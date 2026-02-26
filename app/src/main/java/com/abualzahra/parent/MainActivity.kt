de6de4f54e8bf5964903e2a8ba025661fea30914
mneeralsydy-png <mneeralsydy@gmail.com>
2026-02-27 01:27:45 +0300

    Update MainActivity.kt

--- app/src/main/java/com/abualzahra/parent/MainActivity.kt
+++ app/src/main/java/com/abualzahra/parent/MainActivity.kt
@@ -5,15 +5,10 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
-    private val auth = FirebaseAuth.getInstance()
-    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,26 +16,28 @@ class MainActivity : AppCompatActivity() {
 
         webView = findViewById(R.id.webview)
         setupWebView()
-        
-        // التحقق من حالة المستخدم عند البدء
-        checkUserStatus()
     }
 
     private fun setupWebView() {
         val webSettings: WebSettings = webView.settings
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
         
+        // السماح بالوصول للملفات والأصول
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+
         webView.addJavascriptInterface(WebAppInterface(this), "AndroidNative")
         webView.webViewClient = WebViewClient()
         webView.loadUrl("file:///android_asset/index.html")
     }
 
-    private fun checkUserStatus() {
-        // سيتم التعامل مع الحالة عبر استدعاءات JS بعد تحميل الصفحة
-    }
-
     override fun onBackPressed() {
-        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
