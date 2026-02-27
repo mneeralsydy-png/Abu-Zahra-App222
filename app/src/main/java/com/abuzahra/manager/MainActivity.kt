2c994f98bb1b00791769d9acdc5f0f8e4f393113
mneeralsydy-png <mneeralsydy@gmail.com>
2026-02-27 03:30:29 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/abuzahra/manager/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.abuzahra.manager
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    // تم إزالة private ليصبح public ويمكن لـ WebAppInterface الوصول إليه
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        setupWebView()
+    }
+
+    private fun setupWebView() {
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
+        
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+
+        webView.addJavascriptInterface(WebAppInterface(this), "AndroidNative")
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
